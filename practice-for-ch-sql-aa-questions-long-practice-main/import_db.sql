9d8844e5244ba88b5f6f3f08d59f393e9ac94d25
yuris <yls4@cornell.edu>
2023-09-20 10:42:14 -0400

    created aa questions db by seeding

--- practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
+++ practice-for-ch-sql-aa-questions-long-practice-main/import_db.sql
@@ -5,10 +5,11 @@ DROP TABLE IF EXISTS question_follows;
 DROP TABLE IF EXISTS replies;
 DROP TABLE IF EXISTS question_likes;
 
-CREATE TABLE users (
+
+CREATE TABLE users(
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
 CREATE TABLE questions(
@@ -44,8 +45,38 @@ CREATE TABLE replies(
 CREATE TABLE question_likes(
     id INTEGER PRIMARY KEY,
     question_id INTEGER NOT NULL, 
-    user_id INTEGER NOT NULL 
+    user_id INTEGER NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+INSERT INTO
+    users(fname, lname)
+VALUES
+    ('Yuri', 'Sugihara'),
+    ('Balaji', 'V');
+
+INSERT INTO
+    questions(title, body, associated_author)
+VALUES
+    ('How are you?', 'Just wondering', 2),
+    ('What''s the weather like?', 'Looks like it will rain', 1);
+
+INSERT INTO 
+    question_follows(question_id, user_id)
+VALUES
+    (2, 2),
+    (1, 1),
+    (2, 1),
+    (1, 2);
+
+INSERT INTO
+    replies(question_id, user_id, body)
+VALUES 
+    (1, 1, 'I''m doing well');
+
+INSERT INTO
+    question_likes(question_id, user_id)
+VALUES
+    (1, 1);
